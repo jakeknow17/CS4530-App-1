183001660007316e6ee35fc5fc0cbc144c39aa6d
Jacob Knowlton <knowlton.jacob17@gmail.com>
2023-02-14 17:50:10 -0700

    Added view binding and camera button

--- app/src/main/java/com/example/cs4530app1/MainActivity.kt
+++ app/src/main/java/com/example/cs4530app1/MainActivity.kt
@@ -1,11 +1,36 @@
 package com.example.cs4530app1
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.ActivityNotFoundException
+import android.content.Intent
 import android.os.Bundle
+import android.provider.MediaStore
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
+import com.example.cs4530app1.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.cameraButton.setOnClickListener { onClickPictureButton() }
+    }
+
+    private fun onClickPictureButton() {
+        val cameraIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        try {
+            getPicture.launch(cameraIntent)
+        } catch (e: ActivityNotFoundException) {
+            print(e)
+        }
+    }
+
+    private val getPicture = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+        print(it)
     }
 }
